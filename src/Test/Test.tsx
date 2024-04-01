c445103663adf0913fd2da5ed04c1eaba29d806c
Khuc Phat <anhphat2j@gmail.com>
2024-04-02 06:44:46 +0700

    upload node

--- src/Test/Test.tsx
+++ src/Test/Test.tsx
@@ -1,11 +1,31 @@
-import ParentComponent from "@/pages/TestContext/ParentComponent";
-import React, { useState } from "react";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 const Test = () => {
-  const [value, setValue] = useState(1);
   return (
     <>
-      <ParentComponent value={value} />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={["DatePicker"]}>
+          <DatePicker
+            sx={{
+              "& .MuiOutlinedInput-notchedOutline": {
+                borderWidth: "0px",
+                borderBottom: "1px solid #ccc",
+                borderRadius: "unset",
+              },
+              "& .css-o9k5xi-MuiInputBase-root-MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                {
+                  borderWidth: "0px",
+                  borderBottom: "1px solid #ccc",
+                  borderRadius: "unset",
+                },
+            }}
+            label="Basic date picker"
+          />
+        </DemoContainer>
+      </LocalizationProvider>
     </>
   );
 };
