48edf192218b614a4683c3a3fe47f64173468d6e
khucphat <95363960+KhucPhat@users.noreply.github.com>
2024-06-19 16:22:41 +0700

    Update TestSelectText.tsx

--- src/Test/TestData/TestSelectText.tsx
+++ src/Test/TestData/TestSelectText.tsx
@@ -1,33 +1,45 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
+import { Menu, MenuItem } from '@mui/material';
 
-function AutoSelectText() {
-  const [value, setValue] = useState("Example text to select. Click and drag to test.");
-  const inputRef = useRef(null);
+const TextPopup = ({ children }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedText, setSelectedText] = useState('');
 
-  const handleSelect = () => {
-    const input = inputRef.current;
-    const selection = window.getSelection().toString();
-    if (selection.length > 0) {
-      const start = input.value.indexOf(selection);
-      const end = start + selection.length;
-      
-      // Mở rộng vùng chọn để bao gồm toàn bộ chuỗi con
-      if (start > -1) {
-        input.setSelectionRange(start, end);
-      }
+  const handleRightClick = (event) => {
+    event.preventDefault();
+    const selectedText = window.getSelection().toString();
+    if (selectedText.length > 0) {
+      setSelectedText(selectedText);
+      setAnchorEl(event.currentTarget);
     }
   };
 
-  return (
-    <input
-      type="text"
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-      onSelect={handleSelect}
-      ref={inputRef}
-      style={{ width: '300px' }}
-    />
-  );
-}
+  const handleClose = () => {
+    setAnchorEl(null);
+    setSelectedText('');
+  };
 
-export default AutoSelectText;
+  return (
+    <div onContextMenu={handleRightClick}>
+      <p>Đây là văn bản mà bạn có thể bôi đen và nhấn chuột phải để thấy các tùy chọn.</p>
+      <Menu
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Copy</MenuItem>
+        <MenuItem onClick={handleClose}>Highlight</MenuItem>
+      </Menu>
+    </div>
+  );
+};
+
+export default TextPopup;
